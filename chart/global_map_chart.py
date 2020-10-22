c60145fca4f8e7bb00bcaa597f9cfa47a876ecd9
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-10-21 23:19:03 -0300

    adicionado User nos dados das coordenadas

--- chart/global_map_chart.py
+++ chart/global_map_chart.py
@@ -2,18 +2,18 @@ import plotly.express as px
 
 from data.global_map_data import get_chart_data
 
-df=get_chart_data()
+df = get_chart_data()
 
 fig = px.scatter_mapbox(df, lat="lat", lon="lon",
-                       color="Radiation Level (mR/h)",
-                       size="Radiation Level (mR/h)",
-                       color_continuous_scale=["blue", "yellow", "red"],
-                       size_max=15,
-                       zoom=10,
-                       hover_name='Local'
-                 )
+                        color="Radiation Level (mR/h)",
+                        size="Radiation Level (mR/h)",
+                        color_continuous_scale=["blue", "yellow", "red"],
+                        size_max=15,
+                        zoom=10,
+                        hover_name='Local',
+                        hover_data=['User']
+                        )
 
-fig.update_layout(mapbox_style="carto-positron",)
+fig.update_layout(mapbox_style="carto-positron", )
 
 # fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-
