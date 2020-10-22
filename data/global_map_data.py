08960bac8e98f9ef0df06461dbbd51b93a221c24
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-10-21 23:35:21 -0300

    pegando dados de backup

--- data/global_map_data.py
+++ data/global_map_data.py
@@ -41,8 +41,6 @@ def get_data_from_last_backup():
     data = pd.read_excel('data/botucatu_radiation_latlong.xlsx')
     df = pd.DataFrame(data)
 
-    df[['lat', 'lon']] = df['lat-long'].str.split(',', expand=True)
-
     df['lat'] = df['lat'].astype(dtype='float64')
     df['lon'] = df['lon'].astype(dtype='float64')
 
